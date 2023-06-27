2e6d1ff225336a487f9bcfa2f5fb2f95f83ef985
miwwo <miwwoHG@gmail.com>
2023-06-27 19:17:46 +0300

    fix панель курьера не для админов

--- src/main/java/com/example/buysell/configurations/SecurityConfig.java
+++ src/main/java/com/example/buysell/configurations/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/order/**", "/images/**", "/registration", "/user/**")
+                .antMatchers("/", "/order/**", "/images/**", "/registration", "/user/**", "/carrier/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
